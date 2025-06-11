e15d53302c07235c4f6af3b983daf1cb0111e813
Gustavo <gustavogomes1108@gmail.com>
2025-06-11 15:38:20 -0300

    .

--- script.js
+++ script.js
@@ -5,33 +5,23 @@ fetch("https://api.disneyapi.dev/character?pageSize=24")
 
     let container = document.querySelector(".conteudo");
 
-    let linhaCards = document.createElement("div")
-
-    linhaCards.className = "row gap-2 d-flex justify-content-center align-items-center"
-
     dados.data.forEach((personagem)=>{
-      linhaCards.innerHTML += `
-        <div class="col-xl-2 col-lg-4 col-md-6 col-sm-8 col-12">
-          <div class="card">
-              <div class="card-body p-2 d-flex justify-content-center align-items-center flex-column">
-                  <div class="mb-2">
-                    <img class="img-fluid" src="${personagem.imageUrl}" alt="card-personagem">
-                  </div>
-                  
-                  <div>
-                    <h6>${personagem.name}</h3>
-                    <div>Filmes: ${personagem.films[0]}</div>
-                    <div>Série de TV: ${personagem.tvShows[0]}</div>  
-                  </div>
-              </div>
-          </div>
-      </div>
+      container.innerHTML += `
+        <div class="card">
+            <div class="card-body d-flex justify-content-center align-items-center flex-column">
+                <div class="mb-2">
+                  <img src="${personagem.imageUrl}" alt="card-personagem">
+                </div>
+                
+                <div>
+                  <h6>${personagem.name}</h3>
+                  <div>Filmes: ${personagem.films[0]}</div>
+                  <div>Série de TV: ${personagem.tvShows[0]}</div>  
+                </div>
+            </div>
+        </div>
       `;
-    })
-
-    container.appendChild(linhaCards)
-    
-
+    }) 
   })
 
   .catch((erro) => {
